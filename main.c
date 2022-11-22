181b90c731ec30d826354ebe9c7e70d0f753d2dc
cmy <1829431836@qq.com>
2022-11-22 21:55:17 +0800

    init

---
+++ main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <sys/wait.h>
+#include <sys/socket.h>
+
+int main(int argc, char* argv[])
+{
+    return 0;
+}
+
